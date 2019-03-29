d42da69965edf7c73f0ff3eb76bf24e4dce47598
syfcy <shenyufan2000@outlook.com>
2019-03-29 13:23:59 +0800

    第一次改 除格式问题

--- 临时2.cpp
+++ 临时2.cpp
@@ -1,20 +1,84 @@
-﻿// 临时2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿//hhh 给小傻子看
 #include <iostream>
+using namespace std;
+class Array
+{
+public:
+	Array(int (&p)[4][4], int y);
+	int fun(int n);
+	void intencode();
+	void print();
+	void printcount();
+private:
+	int a[4][4];
+	int count;
+};
+Array::Array(int(&p)[4][4], int y)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+			a[i][j] = p[i][j];
+	}
+	count = y;
+}
+int Array::fun(int n)  //小傻子 判断个素数为啥总是这么复杂呢hhh
+{
+	if (n == 2 || n == 3) return 1;
+	if (n == 1) return 0;
+	int i;
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0) return 0;
+	}
+    return 1; 
+}
+
+void Array::intencode()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j <= i; j++)
+			if (fun(a[i][j]))
+			{
+				count++;
+				int w = a[i][j] + 1;
+				while(fun(w)==0)
+				w++;
+
+				a[i][j] = w;
+			}
+	}
+}
+void Array::print()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		if(i!=3) cout << endl;
+	}
+}
+
+void Array::printcount()
+{
+	cout << endl<<"count="<< count << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int xx[4][4];
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+			cin >> xx[i][j];
+	}
+	Array A(xx, 0);
+	A.print();
+	cout << endl; //为了格式问题 前面不输出 这边单独输出
+	A.intencode();
+	A.printcount(); //重新写一个函数来访问私有数据成员呢
+	A.print();
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
